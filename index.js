aaaf0053738dd49b9b5681bf403a9f94a239a8b5
Siyeon-dev <siyeon.dev@gmail.com>
2021-04-09 09:34:02 +0900

    Handling Routes with Express

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-console.log("Hi!");
+const express = require('express');
+const app = express();
+
+const PORT = 4000;
+
+function handleListening() {
+    console.log(`Listening on: http://localhost:${PORT}`);
+}
+
+function handleHome(req, res) {
+    console.log(req);
+    res.send('Hello from home');
+}
+
+function handleProfile(req, res) {
+    res.send("You are on my profile");
+}
+
+app.get("/", handleHome);
+
+app.get("/profile", handleProfile);
+
+app.listen(PORT, handleListening);
