f0ca7478a34fdec844bcb009188c68cacad75d1c
Siyeon-dev <siyeon.dev@gmail.com>
2021-04-30 10:00:44 +0900

    Search Controller

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -1,5 +1,10 @@
 export const home = (req, res) => res.render("home", { pageTitle : "Home"});
-export const search = (req, res) => res.render("search", { pageTitle : "Search"});
+
+export const search = (req, res) => {
+    const {query: { term: searchingBy }} = req;
+    res.render("search", { pageTitle: "Search", searchingBy}); 
+};
+
 export const videos = (req, res) => res.render("videos", { pageTitle : "Videos"});
 export const upload = (req, res) => res.render("upload", { pageTitle : "Upload"});
 export const video_detial = (req, res) => res.render("videoDetail", { pageTitle : "Video Detail"});
