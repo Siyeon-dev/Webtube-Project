dc8f5871b9d81d0287d018ce2825efb324130fb7
Siyeon-dev <siyeon.dev@gmail.com>
2021-05-01 14:02:58 +0900

    Home Controller part Two

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ import videoRouter from "./routers/videoRouter";
 import globalRouter from "./routers/globalRouter";
 const app = express();
 
-app.use(helmet());
+app.use(helmet({ contentSecurityPolicy: false }));
 app.set("view engine", "pug");
 app.use(cookieParser());
 app.use(bodyParser.json({extended: true}));
