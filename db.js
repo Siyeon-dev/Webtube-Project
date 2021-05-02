7f67708792e4e27f389626732bbd8898d3b1b44c
Siyeon-dev <siyeon.dev@gmail.com>
2021-05-02 16:55:56 +0900

    Connectiong to MongoDB

--- db.js
+++ db.js
@@ -1,54 +1,16 @@
-export const videos = [
-	{
-		id: 324393,
-		title: 'Video awesome',
-		description: 'This is something I love',
-		views: 24,
-		videoFile:
-			'https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4',
-		creator: {
-			id: 121212,
-			name: 'Nicolas',
-			email: 'nico@las.com',
-		},
-	},
-	{
-		id: 1212121,
-		title: 'Video super',
-		description: 'This is something I love',
-		views: 24,
-		videoFile:
-			'https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4',
-		creator: {
-			id: 121212,
-			name: 'Nicolas',
-			email: 'nico@las.com',
-		},
-	},
-	{
-		id: 55555,
-		title: 'Video nice',
-		description: 'This is something I love',
-		views: 24,
-		videoFile:
-			'https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4',
-		creator: {
-			id: 121212,
-			name: 'Nicolas',
-			email: 'nico@las.com',
-		},
-	},
-	{
-		id: 11111,
-		title: 'Video perfect',
-		description: 'This is something I love',
-		views: 24,
-		videoFile:
-			'https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4',
-		creator: {
-			id: 121212,
-			name: 'Nicolas',
-			email: 'nico@las.com',
-		},
-	},
-];
+import mongoose from 'mongoose';
+
+mongoose.connect('mongodb://localhost:27017/we-tube', {
+	useNewUrlParser: true,
+	useFindAndModify: false,
+	useUnifiedTopology: true,
+});
+
+const db = mongoose.connection;
+
+const handleOpen = () => console.log('✅ Connected to DB');
+const handleError = (Error) =>
+	console.log(`❌ Error on DB Connection: ${Error}`);
+
+db.once('open', handleOpen);
+db.on('error', handleError);
