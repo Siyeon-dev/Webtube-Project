aaa3e00e15a5a091a0875b6e97ed183379e568bc
Siyeon-dev <siyeon.dev@gmail.com>
2021-05-03 08:38:30 +0900

    Uploading and Creating a Video part Two

--- app.js
+++ app.js
@@ -1,21 +1,22 @@
-import express from 'express';
-import morgan from 'morgan';
-import helmet from 'helmet';
-import cookieParser from 'cookie-parser';
-import bodyParser from 'body-parser';
-import { localsMiddleware } from './middlewares';
-import routes from './routes';
-import userRouter from './routers/userRouter';
-import videoRouter from './routers/videoRouter';
-import globalRouter from './routers/globalRouter';
+import express from "express";
+import morgan from "morgan";
+import helmet from "helmet";
+import cookieParser from "cookie-parser";
+import bodyParser from "body-parser";
+import { localsMiddleware } from "./middlewares";
+import routes from "./routes";
+import userRouter from "./routers/userRouter";
+import videoRouter from "./routers/videoRouter";
+import globalRouter from "./routers/globalRouter";
 const app = express();
 
 app.use(helmet({ contentSecurityPolicy: false }));
-app.set('view engine', 'pug');
+app.set("view engine", "pug");
+app.use("/uploads", express.static("uploads"));
 app.use(cookieParser());
 app.use(bodyParser.json({ extended: true }));
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(morgan('dev'));
+app.use(morgan("dev"));
 
 app.use(localsMiddleware);
 
