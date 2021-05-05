d81c907f593ec440c2e412724121af2ce7a28295
Siyeon-dev <siyeon.dev@gmail.com>
2021-05-05 20:23:51 +0900

    Searching Videos

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -3,7 +3,7 @@ import Video from "../models/Video";
 
 export const home = async (req, res) => {
 	try {
-		const videos = await Video.find({}.sortStable({ _id: -1 }));
+		const videos = await Video.find({}).sort({ _id: -1 });
 		res.render("home", { pageTitle: "Home", videos });
 	} catch (error) {
 		console.log(error);
@@ -11,10 +11,21 @@ export const home = async (req, res) => {
 	}
 };
 
-export const search = (req, res) => {
+export const search = async (req, res) => {
 	const {
 		query: { term: searchingBy },
 	} = req;
+
+	let videos = [];
+
+	try {
+		videos = await Video.find({
+			title: { $regex: searchingBy, $options: "i" },
+		});
+	} catch (error) {
+		console.log(error);
+	}
+
 	res.render("search", { pageTitle: "Search", searchingBy, videos });
 };
 
